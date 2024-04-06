8207d2fee0e068d59a9453f1e23e8949beb19c39
Tyler Suehr <tylersuehr@gmail.com>
2024-04-06 16:04:23 -0400

    Captures button clicked analytics

--- lib/presentation/components/navigation_buttons.dart
+++ lib/presentation/components/navigation_buttons.dart
@@ -1,3 +1,5 @@
+import 'package:analytics_flutter_example/analytics/analytics.dart';
+import 'package:analytics_flutter_example/analytics/events/button_clicked_event.dart';
 import 'package:analytics_flutter_example/presentation/screens/first_screen/first_screen.dart';
 import 'package:analytics_flutter_example/presentation/screens/second_screen/second_screen.dart';
 import 'package:analytics_flutter_example/presentation/screens/third_screen/thrid_screen.dart';
@@ -13,12 +15,15 @@ class NavigationButtons extends StatelessWidget {
       alignment: MainAxisAlignment.center,
       children: [
         FilledButton(onPressed: () {
+          Analytics.capture(ButtonClickedEvent.screen(context, buttonName: "goto-one-button"));
           context.pushNamed(FirstScreen.screenName);
         }, child: const Text("Goto One")),
         FilledButton(onPressed: () {
+          Analytics.capture(ButtonClickedEvent.screen(context, buttonName: "goto-two-button"));
           context.pushNamed(SecondScreen.screenName);
         }, child: const Text("Goto Two")),
         FilledButton(onPressed: () {
+          Analytics.capture(ButtonClickedEvent.screen(context, buttonName: "goto-three-button"));
           context.pushNamed(ThirdScreen.screenName);
         }, child: const Text("Goto Three")),
       ],
